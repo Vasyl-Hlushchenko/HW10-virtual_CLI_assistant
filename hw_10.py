02131f9b0a6521447c378fe45d4cc5ad21ce0259
Vasyl Hlushchenko <Gluschenkov88@gmail.com>
2022-10-09 13:01:00 +0300

    Add change
    Add change

--- hw_10.py
+++ hw_10.py
@@ -3,16 +3,16 @@ from collections import UserDict
 
 class AddressBook(UserDict):
     #Класс AddressBook, который наследуется от UserDict, и мы потом добавим логику поиска по записям в этот класс.
-    def add_record(self):
-        self.data[Record.name.value] = Record
+    def add_record(self, record):
+        self.data[record.name.value] = record
 
 
 class Record:
     #Класс Record, который отвечает за логику добавления/удаления/редактирования необязательных полей и хранения обязательного поля Name.
-    def __init__(self, name, phone=None):
+    def __init__(self, name, phone = None):
         self.name = Name(name)
         self.phone = Phone(phone)
-        if phone:
+        if Phone(phone):
             self.optional_objects = [Phone(phone)]
         else:
             self.optional_objects = []
@@ -24,23 +24,23 @@ class Record:
         if Phone(phone) in self.optional_objects:
             self.optional_objects.remove(Phone(phone))
 
-    def change_phone(self, phone, new_phone):
-        if Phone(phone) in self.optional_objects:
-            self.optional_objects[self.optional_objects.index(Phone(phone))] = Phone(new_phone)
+    def change_phone(self, phone_for_change, new_phone):
+        print(self.optional_objects.index(Phone(phone_for_change)))
+        if Phone(phone_for_change) in self.optional_objects:
+            self.optional_objects[self.optional_objects.index(Phone(phone_for_change))] = Phone(new_phone)
 
 
 class Field:
     #Класс Field, который будет родительским для всех полей, в нем потом реализуем логику общую для всех полей.
-    pass
+    def __init__(self, value):
+        self.value = value
 
 
 class Name(Field):
     #Класс Name, обязательное поле с именем.
-    def __init__(self, name):
-        self.name = name
+    pass
     
 
 class Phone(Field):
     #Класс Phone, необязательное поле с телефоном и таких одна запись (Record) может содержать несколько.
-    def __init__(self, phone=None):
-        self.phone = phone
+    pass
